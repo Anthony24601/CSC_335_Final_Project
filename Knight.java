7c9799c3e3008db8fc8dc3919c4517b0c9577f2e
Chris Macholtz <chrismacholtz@gmail.com>
2022-11-21 20:44:34 -0700

    basics in terminal, mostly

---
+++ Knight.java
@@ -0,0 +1,114 @@
+import java.util.ArrayList;
+
+public class Knight extends Piece {
+    private static final String FILE_NAME = "knight.png";
+    final static char KIND = Piece.KNIGHT;
+
+     /**
+     * Constructs a new king of the specified color on the
+     * passed Board
+     * @param color either Piece.BLACK or Piece.WHITE
+     * @param b a Board object
+     */
+    public Knight(int color, int rank, int file) {
+        super(color, rank, file);
+    }
+
+    public String[] getValidMoves(Board board) {
+        ArrayList<String> moves = new ArrayList<>();
+        int opColor = color == Piece.WHITE ? Piece.BLACK : Piece.WHITE;
+        boolean isCapture;
+
+        // right-up
+        if (board.isInBounds(rank+1, file+2) && board.isEmpty(rank+1, file+2)) {
+            isCapture = board.get(rank+1, file+2).getColor() == opColor;
+            moves.add(MoveParser.constructMove(this, rank+1, file+2, isCapture));
+        }
+
+        // up-right
+        if (board.isInBounds(rank+2, file+1) && board.isEmpty(rank+2, file+1)) {
+            isCapture = board.get(rank+2, file+1).getColor() == opColor;
+            moves.add(MoveParser.constructMove(this, rank+2, file+1, isCapture));
+        }
+
+        // up-left
+        if (board.isInBounds(rank+2, file-1) && board.isEmpty(rank+2, file-1)) {
+            isCapture = board.get(rank+2, file-1).getColor() == opColor;
+            moves.add(MoveParser.constructMove(this, rank+2, file-1, isCapture));
+        }
+
+        // left-up
+        if (board.isInBounds(rank+1, file-2) && board.isEmpty(rank+1, file-2)) {
+            isCapture = board.get(rank+1, file-2).getColor() == opColor;
+            moves.add(MoveParser.constructMove(this, rank+1, file-2, isCapture));
+        }
+
+        // left-down
+        if (board.isInBounds(rank-1, file-2) && board.isEmpty(rank-1, file-2)) {
+            isCapture = board.get(rank-1, file-2).getColor() == opColor;
+            moves.add(MoveParser.constructMove(this, rank-1, file-2, isCapture));
+        }
+
+        // down-left
+        if (board.isInBounds(rank-2, file-1) && board.isEmpty(rank-2, file-1)) {
+            isCapture = board.get(rank-2, file-1).getColor() == opColor;
+            moves.add(MoveParser.constructMove(this, rank-2, file-1, isCapture));
+        }
+
+        // down-right
+        if (board.isInBounds(rank-2, file+1) && board.isEmpty(rank-2, file+1)) {
+            isCapture = board.get(rank-2, file+1).getColor() == opColor;
+            moves.add(MoveParser.constructMove(this, rank-2, file+1, isCapture));
+        }
+
+        // right-down
+        if (board.isInBounds(rank-1, file+2) && board.isEmpty(rank-1, file+2)) {
+            isCapture = board.get(rank-1, file+2).getColor() == opColor;
+            moves.add(MoveParser.constructMove(this, rank-1, file+2, isCapture));
+        }
+
+        String[] ret = new String[moves.size()];
+        ret = moves.toArray(ret);
+        return ret;
+    }
+
+    @Override
+    public String getPicture(int row, int col) {
+		String path = "images/";
+		if (row%2 == 0) {
+			if (col%2 == 0) {
+                path += "light/";
+			} else {
+                path += "dark/";
+			}
+		} else {
+			if (col%2 == 1) {
+                path += "light/";
+			} else {
+                path += "dark/";
+			}
+		}
+        if(color==Piece.WHITE){
+            path += "white/";
+        }
+        else{
+            path += "black/";
+        }
+        return path+FILE_NAME;
+	}
+   
+    @Override
+    public int getColor() {
+        return color;
+    }
+
+    @Override
+    public boolean isBlank() {
+        return false;
+    }
+
+    @Override
+    public char getKind() {
+        return KIND;
+    }
+}
--- Piece.java
+++ Piece.java
@@ -1,20 +1,97 @@
 
 public abstract class Piece {
+	// colors
+	public final static int BLANK = 0;
+	public final static int WHITE = 10;
+    public final static int BLACK = 20;
+	public final static char PAWN = 0;
+	public final static char BISHOP = 'B';
+	public final static char KNIGHT = 'N';
+	public final static char ROOK = 'R';
+	public final static char QUEEN = 'Q';
+	public final static char KING = 'K';
 
-	String color;
-	String name;
-
-	public Piece(String color, String name) {
+	protected int color;
+	protected int rank;
+	protected int file;
+	
+	public Piece(int color, int rank, int file) {
 		this.color = color;
-		this.name = name;
-	}
-  
-	public String getPicture(int row, int col) {
-		if ((col%2 == 0 && row%2 == 0) || (col%2 == 1 && row%2 == 1)) {
-			return "images/light/" + color + "/" + name + ".png";
-		} else {
-			return "images/dark/" + color + "/" + name + ".png";
-		}
+		this.rank = rank;
+		this.file = file;
 	}
 	
+	abstract public String[] getValidMoves(Board board);
+
+	protected int getRank() {
+		return rank;
+	}
+
+	protected int getFile() {
+		return file;
+	}
+
+	protected void setRank(int rank) {
+		this.rank = rank;
+	}
+
+	protected void setFile(int file) {
+		this.file = file;
+	}
+
+	protected String getLoc() {
+		return String.format("%c%d", file + 'a' - 1, rank);
+	}
+
+	protected void setLoc(String loc) {
+		rank = loc.charAt(0) - 'a';
+		file = Integer.parseInt(loc.substring(1, 2));
+	}
+
+	protected boolean equals(Piece other) {
+		return color == other.color && rank == other.rank && file == other.file;
+	}
+
+	public String toString() {
+		if (color == WHITE) {
+			return "w" + getKind();
+		} else {
+			return "b" + getKind();
+		}
+	}
+
+	abstract public char getKind();
+
+
+
+	/**
+	 * Returns the path to the image for this Piece. The row and col is
+	 * needed to check if the image is for a dark square or a light
+	 * square.
+	 * @param row row this Piece is at
+	 * @param col column this Piece is at
+	 * @return
+	 */
+	abstract public String getPicture(int row, int col);
+
+	/**
+	 * Returns this Piece's color
+	 * @return Piece.BLACK or Piece.WHITE
+	 */
+	abstract public int getColor();
+
+	/**
+	 * @return true if this is a blank piece, false if its a different type
+	 */
+	abstract public boolean isBlank();
+
+	/**
+	 * Checks if a coordinate is within the board's boundries
+	 * @param row
+	 * @param col
+	 * @return
+	 */
+	protected static boolean isInBounds(int row, int col) {
+        return (row >= 0 && row <= Board.MAX_ROW && col >= 0 && col <= Board.MAX_COLUMN);
+    }
 }
