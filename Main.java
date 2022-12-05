815d505940681502e09ebb75b01a777a27636efb
Miles Gendreau <34330980+milesgendreau@users.noreply.github.com>
2022-12-05 15:42:06 -0700

    menu

--- Main.java
+++ Main.java
@@ -1,14 +1,59 @@
 
 public class Main {
-	private static boolean SERVER = true;
+
+	private static String game_file;
+	private static String ai; // maybe change to work w/ player class
+	private static int mode;
+	
 	public static void main(String[] args) {
-		if(SERVER){
-			Server server = new Server(59896);
-			server.openServer();
+		MainMenu menu = new MainMenu();
+		menu.run();
+		
+		switch (mode) {
+		case 1:
+			Main.localGame();
+			break;
+		case 2:
+			Main.networkedGameCreate();
+			break;
+		case 3:
+			Main.networkedGameJoin();
+			break;
 		}
-		else{
-			LocalPlayer player = new LocalPlayer("normal");
-			player.run();
-		}	
+	}
+	
+	public static void setConfig(String game_file, String ai, int mode) {
+		Main.game_file = game_file;
+		Main.ai = ai;
+		Main.mode = mode;
+	} 
+	
+	public static void localGame() {
+		if (game_file != null) { 
+			// TODO load game
+		}
+		
+		// TODO add ai stuff here
+		LocalPlayer player = new LocalPlayer("normal");
+		player.run();
+	}
+	
+	public static void networkedGameCreate() {
+		if (game_file != null) { 
+			// TODO load game
+		}
+		
+		Server server = new Server(59896);
+		server.openServer();
+		
+		NetworkedPlayer player = new NetworkedPlayer("normal");
+		player.open();
+		player.run();
+	}
+	
+	public static void networkedGameJoin() {
+		NetworkedPlayer player = new NetworkedPlayer("normal");
+		player.open();
+		player.run();
 	}
 }
